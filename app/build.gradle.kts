7db792750dc7fd4697fec05fbe64e9daff056f38
mksdzt <max.detchik@gmail.com>
2023-12-04 18:50:56 +0300

    add auth firebase dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation("com.google.firebase:firebase-analytics")
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-auth")
     implementation ("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
